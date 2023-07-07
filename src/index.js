88a036f29dd463cdb39acd74cff93b935fa9d94f
Krista Merikukka <135420464+merikukk@users.noreply.github.com>
2023-07-07 20:01:15 +0000

    Update 5 files

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ document.getElementById("app").innerHTML = `
   
 </div>
 `;
+let link =
+  "https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff";
 
-async function getText(file) {
-  let myObject = await fetch(file);
-  let myText = await myObject.text();
-  myDisplay(myText);
+async function getData(link) {
+  let datas = await fetch(link);
+  let x = await datas.text();
+  fetchData(x);
 }
