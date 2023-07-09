a112b0a9271dce3d2e0f7abb434fa357110f6ad0
Krista Merikukka <135420464+merikukk@users.noreply.github.com>
2023-07-09 12:37:54 +0000

    Update 147 files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { resolve } from "parcel-bundler/lib/utils/localRequire";
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
@@ -6,22 +7,37 @@ document.getElementById("app").innerHTML = `
   
 </div>
 `;
-let link =
-  "https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff";
 
-getData();
-
-async function getData(link) {
-  try {
+async function getData() {
+    let link =
+      "https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff";
     let datas = await fetch(link);
-    let x = await datas.dataset.dimension.Alue.category.label;
-    console.log("Data received");
-    fetchData(x);
-  } catch {
-    console.log(Error);
+    let x = await datas.json();
+    promises().then(() => {
+      console.log("Data received");
+      fetchData(x)
+      }
+}  
+
+function promises() {
+  const promise = new promise ((resolve, reject) => {
+    resolve();
+    })
+    return promise;
   }
-}
 
 function fetchData(x) {
-  document.getElementById("tablein").appendChild(x);
+  const nodeList = document.querySelectorAll(
+    x.dataset.dimension.Alue.category.label
+  );
+  for (let i = 0; i < nodeList.length; i++) {
+    let textnode = document.createTextNode(nodeList(i));
+    document.getElementById("population").appendChild(textnode);
+  }
+
+  /* const textnode = document.createTextNode("Water");
+
+  document.getElementById("population").appendChild(textnode);*/
 }
+
+getData();
